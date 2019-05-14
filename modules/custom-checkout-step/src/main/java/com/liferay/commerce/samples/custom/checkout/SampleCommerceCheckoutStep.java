ce3a313f0e01f58ffc9d80827e15dae1890efc78
Lawrence Lee <lawrence.lee@liferay.com>
2019-05-14 16:10:33 -0500

    Add Custom Checkout Step Sample

---
+++ modules/custom-checkout-step/src/main/java/com/liferay/commerce/samples/custom/checkout/SampleCommerceCheckoutStep.java
@@ -0,0 +1,108 @@
+package com.liferay.commerce.samples.custom.checkout;
+
+import com.liferay.commerce.checkout.web.util.CommerceCheckoutStep;
+import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Liferay
+ */
+@Component(
+	immediate = true,
+	property = {
+		"commerce.checkout.step.name=" + SampleCommerceCheckoutStep.NAME,
+		"commerce.checkout.step.order:Integer=" + (Integer.MAX_VALUE - 101)
+	},
+	service = CommerceCheckoutStep.class
+)
+public class SampleCommerceCheckoutStep implements CommerceCheckoutStep {
+
+	public static final String NAME = "demo";
+
+	@Override
+	public String getLabel(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		return LanguageUtil.get(resourceBundle, getName());
+	}
+
+	@Override
+	public String getName() {
+		return NAME;
+	}
+
+	@Override
+	public boolean isActive(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws Exception {
+
+		return true;
+	}
+
+	@Override
+	public boolean isOrder() {
+		return false;
+	}
+
+	@Override
+	public boolean isSennaDisabled() {
+		return false;
+	}
+
+	@Override
+	public boolean isVisible(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws Exception {
+
+		return isActive(httpServletRequest, httpServletResponse);
+	}
+
+	@Override
+	public void processAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+	}
+
+	@Override
+	public void render(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws Exception {
+
+		_jspRenderer.renderJSP(
+			_servletContext, httpServletRequest, httpServletResponse,
+			"/checkout_step/view.jsp");
+	}
+
+	@Override
+	public boolean showControls(
+		HttpServletRequest httpServletRequest,
+		HttpServletResponse httpServletResponse) {
+
+		return true;
+	}
+
+	@Reference
+	private JSPRenderer _jspRenderer;
+
+	@Reference(target = "(osgi.web.symbolicname=com.liferay.commerce.sample)")
+	private ServletContext _servletContext;
+
+}
